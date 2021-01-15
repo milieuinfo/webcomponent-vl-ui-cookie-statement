9ef9e4ceebc081af58b33852f67e82b2f4c96632
coemans <tom.coemans@vlaanderen.be>
2021-01-15 15:50:02 +0100

    JSESSIONID cookie is altijd van toepassing (#3)

--- src/vl-cookie-statement-all.js
+++ src/vl-cookie-statement-all.js
@@ -1,9 +1,11 @@
 import {VlCookieStatement} from '/src/vl-cookie-statement.js';
 import {VlCookie} from '/src/vl-cookie.js';
 import {VlStickySessionCookie} from '/src/vl-sticky-session-cookie.js';
+import {VlJSessionIdCookie} from '/src/vl-jsessionid-cookie.js';
 
 export {
   VlCookieStatement,
   VlCookie,
   VlStickySessionCookie,
+  VlJSessionIdCookie,
 };
--- src/vl-sticky-session-cookie.js
+++ src/vl-sticky-session-cookie.js
@@ -16,7 +16,7 @@ import {VlCookie} from '/src/vl-cookie.js';
 export class VlStickySessionCookie extends VlCookie {
   constructor() {
     super({
-      title: 'Sticky session',
+      title: 'Persistentie sessie cookie voor betere gebruikerservaring',
       name: 'BIGipServerPool-AUTO-* (vb. "BIGipServerPOOL-AUTO-app=2016879114.37407.0000")',
       purpose: 'Dankzij de cookie kan er verzekerd worden dat verzoeken van een gebruiker voor de duur van een sessie worden gedistribueerd naar de server waarop ze zijn gestart. De cookies worden daarom vaak beschreven als "sticky sessions". Hierdoor kan een betere gebruikerservaring gerealiseerd worden. Bovendien kan de infrastructuur optimaal gebruikt worden.',
       domain: window.location.hostname,
