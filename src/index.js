e092bc6146a94e76ee6093cdcad229c0f0b6d353
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 18:29:49 +0100

    ES export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export * from '../dist/vl-cookie-statement-all.src.js';
+import {VlCookieStatement} from '../dist/vl-cookie-statement.src.js';
+import {VlCookie} from '../dist/vl-cookie.src.js';
+import {VlStickySessionCookie} from '../dist/vl-sticky-session-cookie.src.js';
+
+export {
+  VlCookieStatement,
+  VlCookie,
+  VlStickySessionCookie,
+};
