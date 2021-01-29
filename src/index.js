0eef0fe59811aabc098942c1f7bab382fca79521
Tom Coemans <tom.coemans@gmail.com>
2021-01-29 09:51:41 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import {VlCookieStatement} from '../dist/vl-cookie-statement.src.js';
-import {VlCookie} from '../dist/vl-cookie.src.js';
-import {VlStickySessionCookie} from '../dist/vl-sticky-session-cookie.src.js';
-
-export {
-  VlCookieStatement,
-  VlCookie,
-  VlStickySessionCookie,
-};
+export * from '../dist/vl-cookie-statement-all.src.js';
